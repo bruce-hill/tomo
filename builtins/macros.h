00ec635718f9ce397e46c614190a6c5ed4a9e82a
Bruce Hill <bruce@bruce-hill.com>
2024-05-12 19:47:34 -0400

    Deprecate stack macro

--- builtins/macros.h
+++ builtins/macros.h
@@ -6,6 +6,5 @@
 #include <string.h>
 
 #define heap(x) (__typeof(x)*)memcpy(GC_MALLOC(sizeof(x)), (__typeof(x)[1]){x}, sizeof(x))
-#define stack(x) (__typeof(x)*)((__typeof(x)[1]){x})
 #define tagged(obj_expr, type_name, tag_name) ({ __typeof(obj_expr) obj = obj_expr; \
                                                 obj.$tag == $tag$##type_name##$##tag_name ? &obj.tag_name : NULL; })
