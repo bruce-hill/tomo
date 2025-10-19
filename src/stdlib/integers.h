a97e85655baa105642de383d11e7e7d636ecca94
Bruce Hill <bruce@bruce-hill.com>
2025-10-19 14:11:19 -0400

    Space out imports so they order correctly

--- src/stdlib/integers.h
+++ src/stdlib/integers.h
@@ -3,7 +3,11 @@
 #pragma once
 
 #include "bigint.h" // IWYU pragma: export
-#include "int16.h" // IWYU pragma: export
-#include "int32.h" // IWYU pragma: export
+
 #include "int64.h" // IWYU pragma: export
+
+#include "int32.h" // IWYU pragma: export
+
+#include "int16.h" // IWYU pragma: export
+
 #include "int8.h" // IWYU pragma: export
