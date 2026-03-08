dc0b3921170253af3465d1bf4bccef2380b331c5
Bruce Hill <bruce@bruce-hill.com>
2026-03-08 17:43:11 -0400

    Bugfixes for lists

--- src/stdlib/lists.c
+++ src/stdlib/lists.c
@@ -113,18 +113,23 @@ void List$insert_all(List_t *list, List_t to_insert, Int_t int_index, int64_t pa
         // If we can fit this within the list's preallocated free space, do that:
         list->free -= to_insert.length;
         list->length += to_insert.length;
-        if (index != (int64_t)list->length + 1)
-            memmove((void *)list->data + index * padded_item_size, list->data + (index - 1) * padded_item_size,
-                    (size_t)(((int64_t)list->length - index + (int64_t)to_insert.length - 1) * padded_item_size));
-        for (int64_t i = 0; i < (int64_t)to_insert.length; i++)
+        if (index != (int64_t)list->length + 1) {
+            // Move the data from insertion index onwards forwards
+            void *from = (void *)list->data + (index - 1) * padded_item_size;
+            void *to = (void *)list->data + (index - 1 + (int64_t)to_insert.length) * padded_item_size;
+            int64_t count = (int64_t)list->length - index;
+            memmove(to, from, (size_t)(count * padded_item_size));
+        }
+        for (int64_t i = 0; i < (int64_t)to_insert.length; i++) {
             memcpy((void *)list->data + (index - 1 + i) * padded_item_size, to_insert.data + i * to_insert.stride,
                    (size_t)padded_item_size);
+        }
     } else {
         // Otherwise, allocate a new chunk of memory for the list and populate it:
-        int64_t new_len = (int64_t)list->length + (int64_t)to_insert.length;
-        list->free = MIN(LIST_MAX_FREE_ENTRIES, MAX(8, new_len / 4));
-        void *data = list->atomic ? GC_MALLOC_ATOMIC((size_t)((new_len + list->free) * padded_item_size))
-                                  : GC_MALLOC((size_t)((new_len + list->free) * padded_item_size));
+        uint64_t new_len = list->length + to_insert.length;
+        uint64_t free = MIN(LIST_MAX_FREE_ENTRIES, MAX(8, new_len / 4));
+        void *data = list->atomic ? GC_MALLOC_ATOMIC((size_t)((new_len + free) * (uint64_t)padded_item_size))
+                                  : GC_MALLOC((size_t)((new_len + free) * (uint64_t)padded_item_size));
         void *p = data;
 
         // Copy first chunk of `list` if needed:
@@ -145,7 +150,7 @@ void List$insert_all(List_t *list, List_t to_insert, Int_t int_index, int64_t pa
             memcpy(p, to_insert.data, (size_t)((int64_t)to_insert.length * padded_item_size));
             p += (int64_t)to_insert.length * padded_item_size;
         } else {
-            for (int64_t i = 0; i < index - 1; i++) {
+            for (int64_t i = 0; i < (int64_t)to_insert.length; i++) {
                 memcpy(p, to_insert.data + to_insert.stride * i, (size_t)padded_item_size);
                 p += padded_item_size;
             }
@@ -163,10 +168,14 @@ void List$insert_all(List_t *list, List_t to_insert, Int_t int_index, int64_t pa
                 }
             }
         }
-        list->length = (uint64_t)new_len;
-        list->stride = padded_item_size;
-        list->data = data;
-        list->data_refcount = 0;
+        *list = (List_t){
+            .data = data,
+            .length = (uint64_t)new_len,
+            .free = free,
+            .atomic = list->atomic,
+            .data_refcount = 0,
+            .stride = padded_item_size,
+        };
     }
 }
 
