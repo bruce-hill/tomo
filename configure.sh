f1fd1f3e8c5f7042e202d9e63eee0a7d21ef4efd
Bruce Hill <bruce@bruce-hill.com>
2026-03-14 23:43:07 -0400

    Compat fixes for configure script

--- configure.sh
+++ configure.sh
@@ -1,9 +1,4 @@
-#!/bin/env bash
-
-# if [ "$1" = '--lazy' -a ! -f 'config.mk' ]; then
-#     echo "Already configured!"
-#     exit 0
-# fi
+#!/bin/sh
 
 error() {
     printf "\033[31;1m%s\033[m\n" "$@"
@@ -18,7 +13,6 @@ fi
 printf '\033[1mChoose where to install Tomo (default: %s):\033[m ' "$default_prefix"
 read -r PREFIX
 if [ -z "$PREFIX" ]; then PREFIX="$default_prefix"; fi
-PREFIX="${PREFIX/#\~/$HOME}"
 
 if ! echo "$PATH" | tr ':' '\n' | grep -qx "$PREFIX/bin"; then
     error "Your \$PATH does not include this prefix, so you won't be able to run tomo!" \
@@ -35,7 +29,6 @@ default_cc="cc"
 printf '\033[1mChoose which C compiler to use by default (default: %s):\033[m ' "$default_cc"
 read -r DEFAULT_C_COMPILER
 if [ -z "$DEFAULT_C_COMPILER" ]; then DEFAULT_C_COMPILER="cc"; fi
-DEFAULT_C_COMPILER="${DEFAULT_C_COMPILER/#\~/$HOME}"
 
 printf '\033[1mDo you want to build the compiler with Link Time Optimization (LTO)?\033[m\n\033[2m(This makes building the Tomo compiler slower, but makes running Tomo programs faster)\033[m\n\033[1m[y/N]\033[m '
 read -r USE_LTO
