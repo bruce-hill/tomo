8c903da56ccf134583486bf4ff67b41b5da24aaa
Bruce Hill <bruce@bruce-hill.com>
2026-06-08 14:29:01 -0400

    Add fail() macro

--- src/stdlib/fail.h
+++ src/stdlib/fail.h
@@ -7,3 +7,7 @@
 _Noreturn void fail_text(Text_t message);
 _Noreturn void fail_source(const char *filename, int start, int end, Text_t message);
 Text_t builtin_last_err();
+
+#ifndef fail
+#define fail(...) fail_text(Texts(__VA_ARGS__))
+#endif
